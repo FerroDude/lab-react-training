20838207aebf4887eaa32ae7c6ea60820abfc7b8
Pedro Ferreira <pedroferrodude@hotmail.com>
2021-10-12 17:27:28 +0100

    added iteration 3

--- src/Random.jsx
+++ src/Random.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
 class Random extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      random: 0,
+    };
+  }
   randomNumber = () => {
     const { min, max } = this.props;
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    this.setState({
+      random: Math.floor(Math.random() * (max - min + 1) + min),
+    });
   };
   render = () => {
+    const { min, max } = this.props;
     return (
       <div>
-        <h1>{this.randomNumber}</h1>
+        <p>
+          Click{' '}
+          <strong strong onClick={this.randomNumber}>
+            HERE
+          </strong>{' '}
+          to get a random number between {min} and {max} =>
+          {this.state.random}
+        </p>
       </div>
     );
   };
